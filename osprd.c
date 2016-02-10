9fcb469afa66616b35fc53ef5400cd3df464379e
Brandon Liu <brandonliu@s-169-232-127-96.resnet.ucla.edu>
2016-02-10 00:24:52 -0800

    read/write osprd_process_request added

--- osprd.c
+++ osprd.c
@@ -120,9 +120,35 @@ static void osprd_process_request(osprd_info_t *d, struct request *req)
 	// Consider the 'req->sector', 'req->current_nr_sectors', and
 	// 'req->buffer' members, and the rq_data_dir() function.
 
-	// Your code here.
-	eprintk("Should process request...\n");
 
+//req-> sector is the next sector to submit
+//unsigned long nr_sectors;   /* no. of sectors left to submit */
+//00140        /* no. of sectors left to submit in the current segment */
+//00141        unsigned int current_nr_sectors;
+	if(req->sector + req->current_nr_sectors) {
+		eprintk("Printing to sector that is out of range\n");
+		end_request(req, 0);
+	}
+
+	// Your code here.
+	unsigned request_type;
+	unit8_t *data_ptr;
+
+	request_type = 	rq_data_dir(req);
+	/* d->data is the beginning address of a sector */
+	data_ptr = d->data + (req->sector * SECTOR_SIZE);
+
+	if(request_type == READ) {
+		memcpy((void*) req->buffer, (void*) data_ptr, req->current->nr_sectors * SECTOR_SIZE);
+	}
+	else if (request_type == WRITE) {
+		memcpy((void*) data_ptr, (void*) req->buffer, req->current->nr_sectors * SECTOR_SIZE);
+	}
+	/* not read or write request */
+	else {		
+		eprintk("Neither read nor written\n");
+		end_request(req, 0);
+	}
 	end_request(req, 1);
 }
 
