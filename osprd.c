fc72fe6b16722f535fc01bbda06abc751fcc19a1
Brandon Liu <brandonliu@wifi-131-179-38-224.host.ucla.edu>
2016-02-10 14:22:51 -0800

    fixed typos in osprd.c

--- osprd.c
+++ osprd.c
@@ -128,24 +128,24 @@ static void osprd_process_request(osprd_info_t *d, struct request *req)
 
 	// Your code here.
 	unsigned request_type;
-	unit8_t *data_ptr;
+	uint8_t *data_ptr;
 
 	request_type = 	rq_data_dir(req);
-	/* d->data is the beginning address of a sector */
+	// d->data is the beginning address of a sector 
 	data_ptr = d->data + (req->sector * SECTOR_SIZE);
 
 	if(request_type == READ) {
-		memcpy((void*) req->buffer, (void*) data_ptr, req->current->nr_sectors * SECTOR_SIZE);
+		memcpy((void*) req->buffer, (void*) data_ptr, req->current_nr_sectors * SECTOR_SIZE);
 	}
 	else if (request_type == WRITE) {
-		memcpy((void*) data_ptr, (void*) req->buffer, req->current->nr_sectors * SECTOR_SIZE);
+		memcpy((void*) data_ptr, (void*) req->buffer, req->current_nr_sectors * SECTOR_SIZE);
 	}
-	/* not read or write request */
+	// not read or write request 
 	else {		
 		eprintk("Neither read nor written\n");
 		end_request(req, 0);
 	}
-	end_request(req, 1);
+	end_request(req, 1); 
 }
 
 
